edc27c198f626967ef66739aa33efa4a44411ed1
shkyung <shkyung@hancom.com>
2019-03-20 20:06:18 +0900

    [코드정리] 코드 분리 및 정리
    contributor로 인정안되는 commit을 보니 github계정과 local .git파일의
    email이 다르다 global이 아닌 local email을 등록시켜놓고 테스트해보자.

--- browserEvent/timeEvt/js/first.js
+++ browserEvent/timeEvt/js/first.js
@@ -1,18 +1,22 @@
 var terminateFlag = false,
+  targetNum = 10000000000,
+  delayTime = 2000,
   timer;
 
 function setTimeoutCalc() {
-    timer = setTimeout(calc, 2000);
+    timer = setTimeout(calc, delayTime);
 }
 
 function calc() {
-    console.error("calc start!");
     var i = 0,
       sum = 0;
 
-    while (i < 10000000000) {
+    console.error("calc start!");
+
+    while (i < targetNum) {
         if (terminateFlag) {
             console.error("calc force shutdown! i : ", i);
+            terminateFlag = false;
             return;
         }
 
@@ -26,7 +30,6 @@ function calc() {
 function forceQuit() {
     console.error("set terminateFlag true!");
     terminateFlag = true;
-
 }
 
 function clearTimer() {
@@ -34,10 +37,13 @@ function clearTimer() {
     clearTimeout(timer);
 }
 
-// 1. calc button click -> 2. quit the calc click twice -> 3. clear the timer third time
+// 1. calc div click -> 2. after delay time, "quit the calc" div click -> 3. "clear the timer" div
 // result:
+
 // calc start!
 // ----------------- sum : 49999999990067860000
 // calc end!
 // set terminateFlag true!     (twice)
 // clear timer!                (third time)
+
+// try whatever you want with this example
--- browserEvent/timeEvt/js/second.js
+++ browserEvent/timeEvt/js/second.js
@@ -1,15 +1,19 @@
 var terminateFlag = false,
   targetNum = 10000000000,
+  delayTime = 2000,
   sum = 0,
+  unit = 10000,
   timer;
 
 function setTimeoutCalc() {
-    timer = setTimeout(calc, 2000, 1, 10000);
+    timer = setTimeout(calc, delayTime, 1);
 }
 
+// 1-10000
+// 10001-20000
+// ....
 function calc() {
     var startNumber = arguments[0],
-      unit = arguments[1],
       j = startNumber,
       max = startNumber + unit;
 
@@ -19,13 +23,14 @@ function calc() {
     for (j; j < max; j++) {
         if (terminateFlag || j > targetNum) {
             console.error("calc force shutdown! j : ", j);
+            terminateFlag = false;
             clearTimeout(timer);
             return;
         }
         sum += j;
     }
 
-    timer = setTimeout(calc, 2000, j, 10000);
+    timer = setTimeout(calc, delayTime, j);
 
     console.error("----------------- sum : ", sum);
     console.error("calc end!");
@@ -40,3 +45,8 @@ function clearTimer() {
     console.error("clear timer!");
     clearTimeout(timer);
 }
+
+// second try
+// To stop while calculating in timer callback function, use this way.
+// split calculating range, and set and call setTimeout
+// then you change the terminateFlag, setTimout cb use this changed variable.
